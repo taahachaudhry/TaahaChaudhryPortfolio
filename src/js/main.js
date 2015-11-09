541bff94fc841af3e72ca294f92424c38e974630
Taaha Chaudhry <taaha.chaudhry@idean.com>
2015-11-09 11:09:26 -0800

    Added in about and resume html files; changed hover state of social icons

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,23 @@
 global.jQuery = require('jquery');
 var $ = global.jQuery;
 
+// var $navLink = $('.nav-links a');
+
+// add active class to active linked
+function activeNavigationLink() {
+  // var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/"));
+  // $navLink.each(function(){
+  //   if($(this).attr("href") == pgurl ) {
+  //     $(this).addClass("active");
+  //   }
+  // });
+
+  // $navLink.click(function() {
+  //   $navLink.removeClass('active');
+  //   $(this).addClass('active');
+  // });
+}
+
 $('document').ready(function() {
-
-  var $navLink = $('.nav-links a');
-
-  $navLink.click(function() {
-    $navLink.removeClass('active');
-    $(this).addClass('active');
-  });
-
+  activeNavigationLink();
 });
