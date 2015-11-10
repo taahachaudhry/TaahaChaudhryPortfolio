9aa4031584de0e314fb3ee01f1d3d7f4f926f385
Taaha Chaudhry <taaha.chaudhry@idean.com>
2015-11-10 15:19:25 -0800

    Restructred js and added page preloader gif

--- src/js/main.js
+++ src/js/main.js
@@ -4,32 +4,49 @@ var $ = global.jQuery;
 require('jquery-bridget');
 require('masonry-layout');
 
-// var $navLink = $('.nav-links a');
+$(document).ready(function() {
 
-// add active class to active linked
-function activeNavigationLink() {
-  // var pgurl = window.location.href.substr(window.location.href.lastIndexOf("/"));
-  // $navLink.each(function(){
-  //   if($(this).attr("href") == pgurl ) {
-  //     $(this).addClass("active");
-  //   }
-  // });
-
-  // $navLink.click(function() {
-  //   $navLink.removeClass('active');
-  //   $(this).addClass('active');
-  // });
-}
-
-function projectLayout() {
-  $('.project-list').masonry({
-    itemSelector: '.project-item',
-    columnWidth: '.grid-sizer',
-    gutter: '.gutter-width'
-  });
-}
-
-$('document').ready(function() {
-  activeNavigationLink();
   projectLayout();
+
+  $(window).load(function() {
+    setTimeout(function(){
+      $('.page-loader').fadeOut(500,function() {
+        init();
+      });
+    }, 500);
+	})
+
+  function init() {
+    activeNavigationLink();
+    // projectLayout();
+  }
+
+  // var $navLink = $('.project-list .project-item');
+
+  // add active class to active linked
+  function activeNavigationLink() {
+    // var pgurl = window.location.href.substr(window.location.href.lastIndexOf("#"));
+    // var pgurl = window.location.hash.substr(1);
+    // $navLink.each(function(){
+    //   if($(this).attr("href") == pgurl ) {
+    //     $(this).addClass("active");
+    //   }
+    // });
+
+    // $navLink.click(function() {
+    //   $navLink.removeClass('active');
+    //   $(this).addClass('active');
+    // });
+    var hash = window.location.hash.slice(1);
+    $('.project').hide();
+    $('#' + hash).show();
+  }
+
+  function projectLayout() {
+    $('.project-list').masonry({
+      itemSelector: '.project-item',
+      columnWidth: '.grid-sizer',
+      gutter: '.gutter-width'
+    });
+  }
 });
