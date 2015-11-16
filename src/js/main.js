8291d7e109114452cbad6b6514f1b6808eefac90
Taaha Chaudhry <taaha.chaudhry@idean.com>
2015-11-16 15:42:31 -0800

    Refactored JS - added in routing to different projects

--- src/js/main.js
+++ src/js/main.js
@@ -4,60 +4,42 @@ var $ = global.jQuery;
 require('jquery-bridget');
 require('masonry-layout');
 
+function initBeforeLoad() {
+  initialLayout();
+  projectLayout();
+}
+
+function projectLayout() {
+  $('.project-list').masonry({
+    itemSelector: '.project-item',
+    columnWidth: '.grid-sizer',
+    gutter: '.gutter-width'
+  });
+}
+
+function initialLayout() {
+  $('.project').hide();
+
+  var hash = window.location.hash;
+  hash = hash.replace(/^#/, '');
+
+  if (hash === '') {
+    $('.project-panel').show();
+  } else {
+    $('.project-panel').hide();
+    $("#" + hash).fadeIn();
+  }
+}
+
 $(document).ready(function() {
 
   initBeforeLoad();
 
-  $(window).on("load resize", function() {
+  $(window).on("load resize hashchange", function() {
+    initBeforeLoad();
+    $('.page-loader').css('display', 'block');
     setTimeout(function(){
-      $('.page-loader').fadeOut(500,function() {
-        init();
-      });
+      $('.page-loader').fadeOut(500);
     }, 500);
 	})
-
-  $(window).on("resize", function() {
-    $('.page-loader').css('display', 'block');
-    init();
-	})
-
-  function initBeforeLoad() {
-    projectLayout();
-  }
-
-  function init() {
-    initBeforeLoad();
-    activeNavigationLink();
-  }
-
-  function projectLayout() {
-    $('.project-list').masonry({
-      itemSelector: '.project-item',
-      columnWidth: '.grid-sizer',
-      gutter: '.gutter-width'
-    });
-  }
-
-  var $projectLink = $('.project-list .project-item');
-
-  // add active class to active linked
-  function activeNavigationLink() {
-    // var pgurl = window.location.href.substr(window.location.href.lastIndexOf("#"));
-    // var pgurl = window.location.hash.substr(1);
-    // $navLink.each(function(){
-    //   if($(this).attr("href") == pgurl ) {
-    //     $(this).addClass("active");
-    //   }
-    // });
-
-    // $navLink.click(function() {
-    //   $navLink.removeClass('active');
-    //   $(this).addClass('active');
-    // });
-    $projectLink.on('click', function () {
-      var hash = window.location.hash.slice(1);
-      $('.project').hide();
-      $('#' + hash).show();
-    })
-  }
 });
