c28dca9a28a820a2ae65e32c92690991bc3d92b5
Taaha Chaudhry <taaha.chaudhry@idean.com>
2015-11-18 12:09:16 -0800

    Updated site layout; added content to portfolio project

--- src/js/main.js
+++ src/js/main.js
@@ -43,7 +43,7 @@ function imagePopup() {
       enabled: true, // By default it's false, so don't forget to enable it
 
       duration: 300, // duration of the effect, in milliseconds
-      easing: 'ease-in-out' // CSS transition easing function
+      easing: 'ease-in' // CSS transition easing function
     }
   });
 }
