3a9ad1c7e01a4a0bd3a1739a1f7e7d081e2afe58
Taaha Chaudhry <taaha.chaudhry@idean.com>
2015-11-17 19:27:21 -0800

    Updated content and added image zoom functionality

--- src/js/main.js
+++ src/js/main.js
@@ -35,7 +35,17 @@ function initialLayout() {
 }
 
 function imagePopup() {
-  $('.wireframe-link').magnificPopup({type:'image'});
+  $('.image-link').magnificPopup({
+    type:'image',
+    mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+
+    zoom: {
+      enabled: true, // By default it's false, so don't forget to enable it
+
+      duration: 300, // duration of the effect, in milliseconds
+      easing: 'ease-in-out' // CSS transition easing function
+    }
+  });
 }
 
 $(document).ready(function() {
